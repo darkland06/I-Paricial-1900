d0b25bf67088bd93bda0403fdf7e92ebba05fecd
darkland06 <darlan.reyes@unah.hn>
2023-02-04 00:46:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 70);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":Ingrese numero";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(133, 70);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(140, 20);
+            this.Numero1TextBox.TabIndex = 1;
+            this.Numero1TextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analizar numero";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ejercicio #1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ejercicio 1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Derivar
+            Decimal Numero1 = 0;
+            
+            //Llamar
+            Numero1 = Convert.ToDecimal(Numero1TextBox.Text);
+            //Numero par he impar
+            if (Numero1  % 2 == 0) 
+            {
+                MessageBox.Show("El numero es par");
+            }
+            else
+            {
+                MessageBox.Show("El numero es impar");
+            }
+            //Positivo o Negativo
+            if (Numero1 > 0)
+            {
+                MessageBox.Show("El numero es positivo ( + )");
+            }
+            else
+            {
+                MessageBox.Show("El numero es negativo ( - )");
+            }
+        }
+    }
+}
